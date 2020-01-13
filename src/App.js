01f2c825f1e854ac47272ceaed2e60f3f6ee74f5
karenheyn <karenheyn@gmail.com>
2020-01-12 19:18:34 -0500

    imported form components, added a little bit of initial styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
 import React from "react";
 import "./App.css";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: 200
+    }
+  }
+}));
 function App() {
+  const classes = useStyles();
   return (
     <div className='App'>
-      <h1>this is a test</h1>
+      <form className={classes.root} noValidate autoComplete='off'>
+        <TextField id='outlined-basic' label='Sample size' variant='outlined' />
+        <TextField id='outlined-basic' label='Sample mean' variant='outlined' />
+        <TextField
+          id='outlined-basic'
+          label='Standard deviation'
+          variant='outlined'
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              // checked={state.checkedB}
+              // onChange={handleChange("checkedB")}
+              value='checkedB'
+              color='primary'
+            />
+          }
+          label='Perform hypothesis test'
+        />
+        <TextField
+          id='outlined-basic'
+          label='Hypothesized mean'
+          variant='outlined'
+          disabled
+        />
+        <div>
+          <Button variant='contained' color='primary'>
+            Submit
+          </Button>
+          <Button variant='contained' color='primary'>
+            Reset
+          </Button>
+        </div>
+      </form>
     </div>
   );
 }
